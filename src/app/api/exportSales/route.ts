bd55aa47d05e85ef0904bb3c3f1e88736d1b76c7
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-08-31 16:11:14 +0900

    Refactor sales_data.csv to include additional columns and update SQL query in getSales function

--- src/app/api/exportSales/route.ts
+++ src/app/api/exportSales/route.ts
@@ -6,14 +6,37 @@ import iconv from 'iconv-lite';
 import { getSales } from '@/app/sql/sqls';
 import { format } from 'date-fns';
 import { toZonedTime } from 'date-fns-tz';
+import { sql } from '@vercel/postgres';
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
-    const sales = await getSales();
+    //const sales = await getSales();
+    try {
+      const {rows} =
+        await sql`SELECT * FROM tbl_ticket_purchases;`;
 
-    if (sales.length === 0) {
-      return NextResponse.json({ message: 'No sales data available' });
+        console.log(rows);
+
+      if (rows.length === 0) {
+        return NextResponse.json({ message: 'No sales data available' });
+      }
+
+      const csv = parse(rows);
+      const filePath = path.join(process.cwd(), 'public', 'sales_data.csv');
+  
+      // UTF-8 with BOM
+      const csvWithBom = iconv.encode('\uFEFF' + csv, 'utf-8');
+      fs.writeFileSync(filePath, csvWithBom);
+
+      return NextResponse.json({ message: 'CSV file has been saved', filePath: '/sales_data.csv' });
+    } catch (error) {
+      return NextResponse.json({ error }, { status: 500 });
     }
+
+
+    // if (sales.length === 0) {
+    //   return NextResponse.json({ message: 'No sales data available' });
+    // }
 
     //   const formattedSales = sales.map((sale) => {
     //       const saleDate = toZonedTime(sale.saledate, 'Asia/Tokyo');
