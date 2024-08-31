de36d5aada85fb1f68e49e6f4264cec2d5b1e01b
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-08-31 15:07:04 +0900

    feat: Refactor CSV export functionality and update sales page

--- src/app/api/exportSales/route.ts
+++ src/app/api/exportSales/route.ts
@@ -6,9 +6,8 @@ import iconv from 'iconv-lite';
 import { getSales } from '@/app/sql/sqls';
 import { format } from 'date-fns';
 import { toZonedTime } from 'date-fns-tz';
-import { QueryResultRow as PgQueryResultRow } from 'pg';
 
-interface QueryResultRow {
+interface Sale {
     saledate: string;
     userId: string;
     buyer: string;
@@ -19,35 +18,26 @@ interface QueryResultRow {
 
 export async function GET() {
     try {
-        const salesData: PgQueryResultRow[] = await getSales();
+    //     const sales: Sale[] = await getSales();
         
-        if (salesData.length === 0) {
-            return NextResponse.json({ message: 'No sales data available' });
-        }
-        
-        const sales: QueryResultRow[] = salesData.map((data) => ({
-            saledate: data.saledate as string,
-            userId: data.userId as string,
-            buyer: data.buyer as string,
-            ticket: data.ticket as string,
-            amount: data.amount as number,
-            seller: data.seller as string,
-        }));
-        
-      const formattedSales = sales.map((sale) => {
-          const saleDate = toZonedTime(sale.saledate, 'Asia/Tokyo');
-          return {
-            ...sale,
-            saledate: format(saleDate, 'yyyy-MM-dd HH:mm:ssXXX'),
-          };
-      });
+    //     if (sales.length === 0) {
+    //         return NextResponse.json({ message: 'No sales data available' });
+    //     }
+
+    //   const formattedSales = sales.map((sale) => {
+    //       const saleDate = toZonedTime(sale.saledate, 'Asia/Tokyo');
+    //       return {
+    //         ...sale,
+    //         saledate: format(saleDate, 'yyyy-MM-dd HH:mm:ssXXX'),
+    //       };
+    //   });
   
-      const csv = parse(formattedSales);
-      const filePath = path.join(process.cwd(), 'public', 'sales_data.csv');
+    //   const csv = parse(formattedSales);
+    //   const filePath = path.join(process.cwd(), 'public', 'sales_data.csv');
       
-      // UTF-8 with BOM
-      const csvWithBom = iconv.encode('\uFEFF' + csv, 'utf-8');
-      fs.writeFileSync(filePath, csvWithBom);
+    //   // UTF-8 with BOM
+    //   const csvWithBom = iconv.encode('\uFEFF' + csv, 'utf-8');
+    //   fs.writeFileSync(filePath, csvWithBom);
       
       return NextResponse.json({ message: 'CSV file has been saved', filePath: '/sales_data.csv' });
     } catch (error) {
