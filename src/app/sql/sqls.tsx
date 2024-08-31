d4c80f84b01cd252c59e2217048e5dc884fd79e9
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-08-31 15:15:37 +0900

    Refactor SQL query in getSales function to exclude unnecessary column

--- src/app/sql/sqls.tsx
+++ src/app/sql/sqls.tsx
@@ -25,7 +25,7 @@ export async function putUser(name: string, userid: string, cardid: string) {
 export async function getSales() {
   try {
     const { rows } =
-      await sql`SELECT user_id as userId,buyer_name as buyer,ticket_type as ticket,amount,seller_id as seller FROM tbl_ticket_purchases`;
+      await sql`SELECT user_id as userId FROM tbl_ticket_purchases`;
     console.log("rows:", rows);
     return rows;
   } catch (error) {
